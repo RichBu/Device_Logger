5d17330e1a1bcac80af6411fb96c1b18fd1d0401
RichBu <RichBu@hotmail.com>
2020-03-01 19:23:14 -0600

    Screen for by Machine is done
    route to  /report-bymach-list/  is done.

--- routes/reports.js
+++ routes/reports.js
@@ -370,4 +370,198 @@ router.post('/evt_list', function(req, res, next) {
 
 
 
+class eventByMachRecStoreType {
+	constructor( _event_str, _start_time_str, _end_time_str, _event_duration_utc, on_time_utc, off_time_utc ) {
+		this.event_str = _event_str;
+		this.start_time_str = _start_time_str;
+		this.end_time_str   = _end_time_str;
+		this.event_duration_utc = _event_duration_utc;
+		this.on_time_utc = _on_time_utc;
+		this.off_time_utc = _off_time_utc;
+	}
+  };
+
+router.post('/evm_list', function(req, res, next) {
+	//display list of events by machine
+	console.log('/reports/evm_list route');
+
+	var _startDate = S("").toString();
+	var tempStrIn = S(req.body.startDate).trim() + "*";
+	if( tempStrIn == "*") {
+		_startDate = "*";
+	} else {
+		_startDate = req.body.startDate + "/01/01";
+	};
+	var _startTime = S("").toString();
+	tempStrIn = S(req.body.startTime).trim() + "*";
+	if( tempStrIn == "*") {
+		if(_startDate == "*") {
+			//start date is already an asterisks
+			_startTime = "";
+		} else {
+			_startTime = "00:00:00";  //set to midnight
+		};
+	} else {
+		_startTime = req.body.startTime + ":00:00";
+	};
+
+
+
+	var _endDate = S("").toString();
+	var tempStrIn2 = S(req.body.endDate).trim() + "*";
+	if( tempStrIn2 == "*") {
+		_endDate = "*";
+	} else {
+		_endDate = req.body.endDate + "/01/01";
+	};
+	var _endTime = S("").toString();
+	tempStrIn2 = S(req.body.endTime).trim() + "*";
+	if( tempStrIn2 == "*") {
+		if(_endDate == "*") {
+			//start date is already an asterisks
+			_endTime = "";
+		} else {
+			_endTime = "00:00:00";  //set to midnight
+		};
+	} else {
+		_endTime = req.body.endTime + ":00:00";
+	};
+
+	var _startMach = req.body.startMach;
+	var _endMach = req.body.endMach;
+
+	//find the start string, either a wild card or the start UTC
+	var searchStartStr = "";
+	if (_startDate == "*") {
+		searchStartStr = "0";   //if it is a wild card then search from utc=0
+	} else {
+		//not a wildcard
+		var tempStr = S(_startDate).left(4).toString(); 
+		var startYear = parseInt(tempStr);
+	
+		var tempStr2 = S(_startDate);
+		var startMonth = parseInt(tempStr2.substr(5,2));
+	
+		var tempStr3 = S(_startDate);
+		var startDate = parseInt(tempStr3.substr(8,2));
+	
+		var tempStr4 = S(_startTime);
+		var startHour = parseInt(tempStr4.left(2));
+		var startMin = parseInt(tempStr4.substr(3,2));
+		var startSec = parseInt(tempStr4.substr(6,2));
+		var startDate_utc = Date.UTC(startYear, startMonth, startDate, startHour, startMin, startSec);	
+		searchStartStr = S(startDate_utc).toString();
+		};
+
+		//find the end date string
+		var searchEndStr = "";
+		if (_endDate == "*") {
+			var endDate_utc = Date.UTC(3000, 12, 31, 0, 0, 0);	
+			searchEndStr = S(endDate_utc).toString();
+		} else {
+			//not a wildcard
+			var tempStr = S(_endDate).left(4).toString(); 
+			var endYear = parseInt(tempStr);
+		
+			var tempStr2 = S(_endDate);
+			var endMonth = parseInt(tempStr2.substr(5,2));
+		
+			var tempStr3 = S(_endDate);
+			var endDate = parseInt(tempStr3.substr(8,2));
+		
+			var tempStr4 = S(_endTime);
+			var endHour = parseInt(tempStr4.left(2));
+			var endMin = parseInt(tempStr4.substr(3,2));
+			var endSec = parseInt(tempStr4.substr(6,2));
+			var endDate_utc = Date.UTC(endYear, endMonth, endDate, endHour, endMin, endSec);	
+			searchEndStr = S(endDate_utc).toString();
+			};
+	
+	var eventByMachListOutput = [];  //array for use in listing
+
+	console.log("start = " + searchStartStr);
+	console.log("end = "   + searchEndStr);
+
+	//check if logged in, later feature
+	//for now, bypass
+	let noLogin = true;
+	if (noLogin || req.session.logged_in === true ) {
+		var actionDone = 'events by mach';		
+		var actionString = 'events by mach';
+		
+		let userLogRec = new userLogRecStoreType(
+			moment().format("YYYY-MM-DD  HH:mm a"),
+			req.session.clientIP,
+			actionDone,
+			actionString
+		);
+
+
+		var query = "INSERT INTO user_log (time_str, ip_addr, action_done, action_string) VALUES (?, ?, ?, ? )";
+		connection.query(query, [
+		  userLogRec.timeStr,
+		  userLogRec.clientIP,
+		  userLogRec.action_done,
+		  userLogRec.action_string
+		  ], function (err, response) {
+		  //log has been written, read all the events
+
+		  var queryStr = "SELECT * FROM event_bymach WHERE on_time_utc >= ? AND on_time_utc <= ?";
+	  
+		  connection.query(queryStr, [searchStartStr, searchEndStr], function (err, response) {
+			  //all of the sessions of previous times pulled out
+			  //console.log(response);
+			  for (var i = 0; i < response.length; i++) {
+				  //loop thru all of the responses
+				  //console.log(response);
+				  //console.log(response[i]);
+				  let dur_hr = 0;
+				  dur_hr = response[i].event_duration_utc/1000/60/60;  //convert to hours 
+				  let on_hr = 0;
+				  on_hr = response[i].on_time_utc/1000/60/60;
+				  let off_hr = 0;
+				  off_hr = response[i].off_time_utc/1000/60/60;
+				  let eventByMachRec = new eventByMachRecStoreType(
+					  response[i].event_str,
+					  response[i].start_time_str,
+					  response[i].end_time_str,
+					  dur_hr,
+					  on_hr,
+					  off_hr
+				  );
+
+				  eventByMachListOutput.push(eventByMachRec);
+			  };
+			  //console.log(videoListOutput);
+			  res.render('report_evm_list', {outputObj: eventByMachListOutput} );
+			  //connection.end();
+		  }); //query for read logfiles  
+		});  //query to write to user log	
+	} else {
+		var actionDone = 'log file list';		
+		let userLogRec = new userLogRecStoreType(
+			moment().format("YYYY-MM-DD  HH:mm a"),
+			req.session.clientIP,
+			'timed out',
+			' ',
+			' ',
+			actionDone
+		);
+	
+		var query = "INSERT INTO user_log (time_str, ip_addr, loginName, password, fullName, action_done) VALUES (?, ?, ?, ?, ?, ? )";
+		connection.query(query, [
+		  userLogRec.timeStr,
+		  userLogRec.clientIP,
+		  userLogRec.loginName,
+		  userLogRec.password,
+		  userLogRec.fullName,
+		  userLogRec.action_done
+		  ], function (err, response) {
+			res.render('index');
+		  });
+	};
+});
+
+
+
 module.exports = router;
