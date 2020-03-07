1bde980bd1e38664c5ff74f0276a00e4d4c95182
RichBu <RichBu@hotmail.com>
2020-03-07 08:26:28 -0600

    Start on mach utilization report
    Add in machine utilization post route and start calcs.
    
    need new hjs file and finish calcs

--- routes/reports.js
+++ routes/reports.js
@@ -381,6 +381,8 @@ class eventByMachRecStoreType {
 	}
   };
 
+
+
 router.post('/evm_list', function(req, res, next) {
 	//display list of events by machine
 	console.log('/reports/evm_list route');
@@ -644,6 +646,270 @@ router.post('/evm_list', function(req, res, next) {
 	};
 });
 
+
+
+
+router.post('/mach_util', function(req, res, next) {
+	//machine utilization report
+	console.log('/reports/evm_list route');
+
+	var _startDate = S("").toString();
+	var tempStrIn = S(req.body.startDate).trim() + "*";
+	if( tempStrIn == "*") {
+		_startDate = "*";
+	} else {
+		_startDate = req.body.startDate + "/01/01";
+	};
+	var _startTime = S("").toString();
+	tempStrIn = S(req.body.startTime).trim() + "*";
+	if( tempStrIn == "*") {
+		if(_startDate == "*") {
+			//start date is already an asterisks
+			_startTime = "";
+		} else {
+			_startTime = "00:00:00";  //set to midnight
+		};
+	} else {
+		_startTime = req.body.startTime + ":00:00";
+	};
+
+
+	var _endDate = S("").toString();
+	var tempStrIn2 = S(req.body.endDate).trim() + "*";
+	if( tempStrIn2 == "*") {
+		_endDate = "*";
+	} else {
+		_endDate = req.body.endDate + "/01/01";
+	};
+	var _endTime = S("").toString();
+	tempStrIn2 = S(req.body.endTime).trim() + "*";
+	if( tempStrIn2 == "*") {
+		if(_endDate == "*") {
+			//start date is already an asterisks
+			_endTime = "";
+		} else {
+			_endTime = "00:00:00";  //set to midnight
+		};
+	} else {
+		_endTime = req.body.endTime + ":00:00";
+	};
+
+	var _startMach = req.body.startMach;
+	var _endMach = req.body.endMach;
+
+	//find the start string, either a wild card or the start UTC
+	var searchStartStr = "";
+	if (_startDate == "*") {
+		searchStartStr = "0";   //if it is a wild card then search from utc=0
+	} else {
+		//not a wildcard
+		var tempStr = S(_startDate).left(4).toString(); 
+		var startYear = parseInt(tempStr);
+	
+		var tempStr2 = S(_startDate);
+		var startMonth = parseInt(tempStr2.substr(5,2));
+	
+		var tempStr3 = S(_startDate);
+		var startDate = parseInt(tempStr3.substr(8,2));
+	
+		var tempStr4 = S(_startTime);
+		var startHour = parseInt(tempStr4.left(2));
+		var startMin = parseInt(tempStr4.substr(3,2));
+		var startSec = parseInt(tempStr4.substr(6,2));
+		var startDate_utc = Date.UTC(startYear, startMonth, startDate, startHour, startMin, startSec);	
+		searchStartStr = S(startDate_utc).toString();
+		};
+
+		//find the end date string
+		var searchEndStr = "";
+		if (_endDate == "*") {
+			var endDate_utc = Date.UTC(3000, 12, 31, 0, 0, 0);	
+			searchEndStr = S(endDate_utc).toString();
+		} else {
+			//not a wildcard
+			var tempStr = S(_endDate).left(4).toString(); 
+			var endYear = parseInt(tempStr);
+		
+			var tempStr2 = S(_endDate);
+			var endMonth = parseInt(tempStr2.substr(5,2));
+		
+			var tempStr3 = S(_endDate);
+			var endDate = parseInt(tempStr3.substr(8,2));
+		
+			var tempStr4 = S(_endTime);
+			var endHour = parseInt(tempStr4.left(2));
+			var endMin = parseInt(tempStr4.substr(3,2));
+			var endSec = parseInt(tempStr4.substr(6,2));
+			var endDate_utc = Date.UTC(endYear, endMonth, endDate, endHour, endMin, endSec);	
+			searchEndStr = S(endDate_utc).toString();
+			};
+	
+	var eventByMachListOutput = [];  //array for use in listing
+
+	console.log("start = " + searchStartStr);
+	console.log("end = "   + searchEndStr);
+
+	//check if logged in, later feature
+	//for now, bypass
+	let noLogin = true;
+	if (noLogin || req.session.logged_in === true ) {
+		var actionDone = 'events by mach';		
+		var actionString = 'events by mach';
+		
+		let userLogRec = new userLogRecStoreType(
+			moment().format("YYYY-MM-DD  HH:mm a"),
+			req.session.clientIP,
+			actionDone,
+			actionString
+		);
+
+
+		var query = "INSERT INTO user_log (time_str, ip_addr, action_done, action_string) VALUES (?, ?, ?, ? )";
+		connection.query(query, [
+		  userLogRec.timeStr,
+		  userLogRec.clientIP,
+		  userLogRec.action_done,
+		  userLogRec.action_string
+		  ], function (err, response) {
+		  //log has been written, read all the events
+
+		  var queryStr = "SELECT * FROM event_bymach WHERE start_time_utc >= ? AND start_time_utc <= ?";
+		  connection.query(queryStr, [searchStartStr, searchEndStr], function (err, response) {
+			  //all of the sessions of previous times pulled out
+			  let currMach = 0;
+			  let currOnTim = 0.0;
+			  let currOffTim = 0.0;
+			  let currTotTim = 0.0;  //total time
+			  let currOnPer = 0.0;
+			  let currOffPer = 0.0;
+
+			  if (response.length>0){
+			 	//might want to put into a class to keep it together
+			  	currMach = parseInt(response[0].mach_num);    //cur mach
+			  };
+
+			  for (var i = 0; i < response.length; i++) {
+				  //loop thru all of the response
+				  let dur_hr = 0;
+				  dur_hr = response[i].event_duration_utc/1000/60/60;  //convert to hours 
+				  let on_hr = 0;
+				  on_hr = response[i].on_time_utc/1000/60/60;
+				  let off_hr = 0;
+				  off_hr = response[i].off_time_utc/1000/60/60;
+
+				  //calculate the percentages
+				  let respMachNum = parseInt(response[i].mach_num);
+				  if ((respMachNum == currMach) && (i < (response.length-1))) {
+					  //mach number has not changed
+					  currOnTim = currOnTim + on_hr;
+					  currOffTim = currOffTim + off_hr;
+					  currTotTim = currTotTim + dur_hr;
+				  } else {
+					  //mach number changed or the last record
+					  if (i == (response.length-1)){
+						//store the last record
+						//make it a different variable then the rest
+						let eventByMachRec2 = new eventByMachRecStoreType(
+							response[i].event_str,
+							response[i].start_time_str,
+							response[i].end_time_str,
+							dur_hr.toFixed(3),
+							on_hr.toFixed(3),
+							off_hr.toFixed(3)
+						  );
+						eventByMachListOutput.push(eventByMachRec2);
+						};
+					  let eventByMachRec = new eventByMachRecStoreType(
+						"totals",
+						" ",
+						" ",
+						currTotTim.toFixed(3),
+						currOnTim.toFixed(3),
+						currOffTim.toFixed(3)
+					  );
+					  eventByMachListOutput.push(eventByMachRec); //store the totals	  
+					  let currZero = 0;
+			  
+					  if (currOnTim == 0) {
+						  currOnPer = 0;
+		 			  } else {
+						currOnPer = (currOnTim/currTotTim) * 100.0;
+					  };
+					  if (currOffTim == 0) {
+						currOffPer = 0;
+					  } else {
+						currOffPer = (currOffTim/currTotTim) * 100.0;
+					  };
+					  
+					  eventByMachRec = new eventByMachRecStoreType(
+						"percentages",
+						" ",
+						" ",
+						//currZero.toFixed(1),
+						" ",
+						currOnPer.toFixed(2),
+						currOffPer.toFixed(2)
+					  );
+					  
+					  eventByMachListOutput.push(eventByMachRec); //store the totals
+
+					  eventByMachRec = new eventByMachRecStoreType(
+						" ",
+						" ",
+						" ",
+						" ",
+						" ",
+						" "
+					  );
+					  eventByMachListOutput.push(eventByMachRec); //store the totals
+				  
+					  currMach =  parseInt(response[i].mach_num);
+					  currTotTim = dur_hr;
+					  currOnTim = on_hr;
+					  currOffTim = off_hr;
+				  };
+				  if (i != (response.length-1)){
+					//store record except if it is the last one
+					let eventByMachRec = new eventByMachRecStoreType(
+						response[i].event_str,
+						response[i].start_time_str,
+						response[i].end_time_str,
+						dur_hr.toFixed(3),
+						on_hr.toFixed(3),
+						off_hr.toFixed(3)
+					  );
+					eventByMachListOutput.push(eventByMachRec);
+					};
+			  	};
+			  //console.log(videoListOutput);
+			  res.render('report_evm_list', {outputObj: eventByMachListOutput} );
+			  //connection.end();
+		  }); //query for read logfiles  
+		});  //query to write to user log	
+	} else {
+		var actionDone = 'log file list';		
+		let userLogRec = new userLogRecStoreType(
+			moment().format("YYYY-MM-DD  HH:mm a"),
+			req.session.clientIP,
+			'timed out',
+			' ',
+			' ',
+			actionDone
+		);
+	
+		var query = "INSERT INTO user_log (time_str, ip_addr, loginName, password, fullName, action_done) VALUES (?, ?, ?, ?, ?, ? )";
+		connection.query(query, [
+		  userLogRec.timeStr,
+		  userLogRec.clientIP,
+		  userLogRec.loginName,
+		  userLogRec.password,
+		  userLogRec.fullName,
+		  userLogRec.action_done
+		  ], function (err, response) {
+			res.render('index');
+		  });
+	};
+});
 
 
 module.exports = router;
