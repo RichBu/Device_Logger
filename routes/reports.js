fb6e683b82bf8d7cd4131308579fa8e2264c2933
RichBu <RichBu@hotmail.com>
2020-03-14 07:26:33 -0500

    Dates on mach utilization
    Working on dates for machine utilization.

--- routes/reports.js
+++ routes/reports.js
@@ -649,18 +649,34 @@ router.post('/evm_list', function(req, res, next) {
 
 
 class MachUtilRecType {
-	constructor( _machOnHours, _machOffHours, _machPerUsage, 
+	constructor( _machOnHours, _machOffHours, _machPerUsage, _machPerIdle, 
 		_totHours, _totPerUtil, _workHours, _workPer) {
 		this.machOnHours = _machOnHours;
 		this.machOffHours = _machOffHours;
 		this.machPerUsage = _machPerUsage;
+		this.machPerIdle = _machPerIdle;
 		this.totHours = _totHours;
 		this.totPerUtil = _totPerUtil;
 		this.workHours = _workHours;
 		this.workPer = _workPer;
-	}
+	} //for storing stats per machine
   };
 
+
+  class MachUtilOutputType {
+	constructor( _descrip, _m1, _m2, _m3, _m4, _m5, _m6, _m7, _m8, _m9 ) {
+		this.Descrip = _descrip;
+		this.M1 = _m1;
+		this.M2 = _m2;
+		this.M3 = _m3;
+		this.M4 = _m4;
+		this.M5 = _m5;
+		this.M6 = _m6;
+		this.M7 = _m7;
+		this.M8 = _m8;
+		this.M9 = _m9;
+	} //for storing stats per machine
+  };
 
 router.post('/mach_util', function(req, res, next) {
 	//machine utilization report
@@ -675,7 +691,7 @@ router.post('/mach_util', function(req, res, next) {
 
 	//fill the table with 0's before starting
 	for (i=0; i<numMach; i++) {
-		let machUtilRec = new MachUtilRecType(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+		let machUtilRec = new MachUtilRecType(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 		machUtilTable.push(machUtilRec);
 	};
 
@@ -724,6 +740,10 @@ router.post('/mach_util', function(req, res, next) {
 	var _endMach = req.body.endMach;
 
 	//find the start string, either a wild card or the start UTC
+	var startDate_utc = 0;
+	var reportStartDate_unix = 0;
+	var searchStartDate_unix = 0;
+	var startDate_str = ""; //start date as a string
 	var searchStartStr = "";
 	if (_startDate == "*") {
 		searchStartStr = "0";   //if it is a wild card then search from utc=0
@@ -742,35 +762,45 @@ router.post('/mach_util', function(req, res, next) {
 		var startHour = parseInt(tempStr4.left(2));
 		var startMin = parseInt(tempStr4.substr(3,2));
 		var startSec = parseInt(tempStr4.substr(6,2));
-		var startDate_utc = Date.UTC(startYear, startMonth, startDate, startHour, startMin, startSec);	
+		startDate_utc = Date.UTC(startYear, startMonth, startDate, startHour, startMin, startSec);	
+		startDate_str = moment().unix(startDate_utc);
+		reportStartDate_unix = moment().unix(startDate_utc);
+		searchStartDate_unix = moment().unix(startDate_utc);
 		searchStartStr = S(startDate_utc).toString();
 		};
 
-		//find the end date string
-		var searchEndStr = "";
-		if (_endDate == "*") {
-			var endDate_utc = Date.UTC(3000, 12, 31, 0, 0, 0);	
-			searchEndStr = S(endDate_utc).toString();
-		} else {
-			//not a wildcard
-			var tempStr = S(_endDate).left(4).toString(); 
-			var endYear = parseInt(tempStr);
+	//find the end date string
+	var endDate_utc = 0;
+	var reportEndDate_unix = 0;
+	var searchEndDate_unix = 0;
+	var endDate_str = "";      //end date in string form
+	var searchEndStr = "";
+	if (_endDate == "*") {
+		endDate_utc = Date.UTC(3000, 12, 31, 0, 0, 0);	
+		searchEndStr = S(endDate_utc).toString();
+	} else {
+		//not a wildcard
+		var tempStr = S(_endDate).left(4).toString(); 
+		var endYear = parseInt(tempStr);
 		
-			var tempStr2 = S(_endDate);
-			var endMonth = parseInt(tempStr2.substr(5,2));
+		var tempStr2 = S(_endDate);
+		var endMonth = parseInt(tempStr2.substr(5,2));
 		
-			var tempStr3 = S(_endDate);
-			var endDate = parseInt(tempStr3.substr(8,2));
+		var tempStr3 = S(_endDate);
+		endDate = parseInt(tempStr3.substr(8,2));
 		
-			var tempStr4 = S(_endTime);
-			var endHour = parseInt(tempStr4.left(2));
-			var endMin = parseInt(tempStr4.substr(3,2));
-			var endSec = parseInt(tempStr4.substr(6,2));
-			var endDate_utc = Date.UTC(endYear, endMonth, endDate, endHour, endMin, endSec);	
-			searchEndStr = S(endDate_utc).toString();
-			};
+		var tempStr4 = S(_endTime);
+		var endHour = parseInt(tempStr4.left(2));
+		var endMin = parseInt(tempStr4.substr(3,2));
+		var endSec = parseInt(tempStr4.substr(6,2));
+		endDate_utc = Date.UTC(endYear, endMonth, endDate, endHour, endMin, endSec);	
+		endDate_str = moment().unix(endDate_utc);
+		reportEndDate_unix = moment().unix(endDate_utc);
+		searchEndDate_unix = moment().unix(endDate_utc);
+		searchEndStr = S(endDate_utc).toString();
+	};
 	
-	var eventByMachListOutput = [];  //array for use in listing
+	var machUtilOutput = []; //mach utilization array for output 
 
 	//check if logged in, later feature
 	//for now, bypass
@@ -799,22 +829,25 @@ router.post('/mach_util', function(req, res, next) {
 		  var queryStr = "SELECT * FROM event_bymach WHERE start_time_utc >= ? AND start_time_utc <= ?";
 		  connection.query(queryStr, [searchStartStr, searchEndStr], function (err, response) {
 			  //all of the sessions of previous times pulled out
-			  let currMach = 0;
-			  let currOnTim = 0.0;
-			  let currOffTim = 0.0;
-			  let currTotTim = 0.0;  //total time
-			  let currOnPer = 0.0;
-			  let currOffPer = 0.0;
-
 			  if (response.length>0){
 			 	//might want to put into a class to keep it together
 			  	currMach = parseInt(response[0].mach_num);    //cur mach
-			  };
+				startDate_utc = response[0].start_time_utc;
+				endDate_utc = response[response.length-1].end_time_utc;
+				};
 
+			  reportStartDate_unix = moment(Date.UTC(4000, 01, 01)).unix;	
+			  reportEndDate_unix = 0;	
 			  for (var i = 0; i < response.length; i++) {
 				  //loop thru all of the response
 				  //add it to the MachUtilTable
 				  let respMachNum = parseInt(response[i].mach_num);
+
+				  var recStartTime_unix = moment(response[i].start_time_str).unix();
+				  var recEndTime_unix = moment(response[i].response[i].end_time_str).unix();
+				  if (recStartTime_unix < reportStartDate_unix)  reportStartDate_unix = recStartTime_unix;
+				  if (recEndTime_unix > reportEndDate_unix) reportEndDate_unix = recEndTime_unix;
+				  if (recStartTime_unix > reportEndDate_unix) reportEndDate_unix = recStartTime_unix;
 
 				  let dur_hr = 0;
 				  dur_hr = response[i].event_duration_utc/1000/60/60;  //convert to hours 
@@ -826,117 +859,214 @@ router.post('/mach_util', function(req, res, next) {
 				  machUtilTable[respMachNum-1].machOffHours = machUtilTable[respMachNum-1].machOffHours + off_hr;
 			  };
 
-			  let totAvailHrs = endDate_utc - startDate_utc;
+			  let totAvailHrs = (reportEndDate_unix - reportStartDate_unix);
+console.log("end date = " + moment(reportEndDate_unix));
+console.log("start date = " + moment(reportStartDate_unix));			  
+console.log("tot avail hours #1 = " + totAvailHrs);			  
+			  totAvailHrs = totAvailHrs/1000/60/60;
+console.log("tot avail hours = " + totAvailHrs);			  
+			  //calc number of working days
+			  var startDayTest = moment.unix(startDate_utc/1000);
+			  var endDayTest = moment.unix(endDate_utc/1000);
+console.log("Start End Day:" );
+console.log(startDayTest);
+console.log(endDayTest);
+console.log(startDayTest <= endDayTest);
+console.log(startDayTest > endDayTest);	
+console.log(startDayTest.format("MM/DD/YYYYTHH:mm:SS"));		  
+console.log(endDayTest.format("MM/DD/YYYYTHH:mm:SS"));		  
+			  //.format("");  
+			  var day = new Date(startDate_utc);
+			  var businessDays = 0;
+			  var endDate = new Date(endDate_utc);
+			  
+console.log("counting num of business days");
+console.log("day gettime = " + day );
+console.log("end date = " + endDate );	
+
+console.log( day<= endDate);
+console.log( "sub tract");
+console.log( day-endDate);
+console.log(startDate_utc - endDate_utc);
+			  while (day <= endDate ) {
+console.log(day);				  
+				  if (day.day()!=0 && day.day()!=6) businessDays++;
+				  day.add(1,'d');
+				}
+				
+console.log("business day =" + businessDays);				
+			  let prodHours = businessDays * 24.0;
+
 			  //different than other report, can calculate pecentages when done
 			  for (var i=0; i<numMach; i++){
 				machUtilTable[i].totHours = totAvailHrs;
+				machUtilTable[i].workHours = prodHours;
 				if (machUtilTable[i].machOnHours == 0){
 					machUtilTable[i].machPerUsage = 0;
+					machUtilTable[i].machPerIdle = 100.0;
 					machUtilTable[i].totPerUtil = 0.0;
+					machUtilTable[i].workPer = 0.0;
 				} else {
 					if (machUtilTable[i].machOffHours == 0){
 						machUtilTable[i].machPerUsage = 100.0;
+						machUtilTable[i].machPerIdle = 0.0;
 						machUtilTable[i].totPerUtil = 100.0;
+						machUtilTable[i].workPer = 100.0;
 					} else {
-						machUtilTable[i].machPerUsage = machUtilTable[i].machOnHours /  machUtilTable[i].machOnHours * 100.0;
+						machUtilTable[i].machPerUsage = machUtilTable[i].machOnHours /  (machUtilTable[i].machOnHours+machUtilTable[i].machOffHours) * 100.0;
+						machUtilTable[i].machPerIdle = 100.0 - machUtilTable[i].machPerUsage;
 						machUtilTable[i].totPerUtil = machUtilTable[i].machOnHours /  machUtilTable[i].totHours * 100.0;
+						machUtilTable[i].workPer = machUtilTable[i].machOnHours /  machUtilTable[i].workHours * 100.0;
 					};   
 				};
 			  };
-				  //calculate the percentages
-				  if ((respMachNum == currMach) && (i < (response.length-1))) {
-					  //mach number has not changed
-					  currOnTim = currOnTim + on_hr;
-					  currOffTim = currOffTim + off_hr;
-					  currTotTim = currTotTim + dur_hr;
-				  } else {
-					  //mach number changed or the last record
-					  if (i == (response.length-1)){
-						//store the last record
-						if (machUtilTable[respMachNum-1].machOnHours == 0){
-							machUtilTable[respMachNum-1].machPerUsage = 0;
-						} else {
-							if (machUtilTable[respMachNum-1].machOffHours == 0)
-							  machUtilTable[respMachNum-1].machPerUsage = 100;
-							else   
-							  machUtilTable[respMachNum-1].machPerUsage = machUtilTable[respMachNum-1].machOnHours /  machUtilTable[respMachNum-1].machOnHours * 100.0;
-						};
+console.log("out of loop");			  
+			  //now start the output
+			  //first record
+			  let machUtilOutputRec = new MachUtilOutputType(
+				  "total on hrs",
+				  machUtilTable[0].machOnHours.toFixed(2),
+				  machUtilTable[1].machOnHours.toFixed(2),
+				  machUtilTable[2].machOnHours.toFixed(2),
+				  machUtilTable[3].machOnHours.toFixed(2),
+				  machUtilTable[4].machOnHours.toFixed(2),
+				  machUtilTable[5].machOnHours.toFixed(2),
+				  machUtilTable[6].machOnHours.toFixed(2),
+				  machUtilTable[7].machOnHours.toFixed(2),
+				  machUtilTable[8].machOnHours.toFixed(2)
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
 
-						//make it a different variable then the rest
-						let eventByMachRec2 = new eventByMachRecStoreType(
-							response[i].event_str,
-							response[i].start_time_str,
-							response[i].end_time_str,
-							dur_hr.toFixed(3),
-							on_hr.toFixed(3),
-							off_hr.toFixed(3)
-						  );
-						eventByMachListOutput.push(eventByMachRec2);
-						};
-					  let eventByMachRec = new eventByMachRecStoreType(
-						"totals",
-						" ",
-						" ",
-						currTotTim.toFixed(3),
-						currOnTim.toFixed(3),
-						currOffTim.toFixed(3)
-					  );
-					  eventByMachListOutput.push(eventByMachRec); //store the totals	  
-					  let currZero = 0;
-			  
-					  if (currOnTim == 0) {
-						  currOnPer = 0;
-		 			  } else {
-						currOnPer = (currOnTim/currTotTim) * 100.0;
-					  };
-					  if (currOffTim == 0) {
-						currOffPer = 0;
-					  } else {
-						currOffPer = (currOffTim/currTotTim) * 100.0;
-					  };
-					  
-					  eventByMachRec = new eventByMachRecStoreType(
-						"percentages",
-						" ",
-						" ",
-						//currZero.toFixed(1),
-						" ",
-						currOnPer.toFixed(2),
-						currOffPer.toFixed(2)
-					  );
-					  
-					  eventByMachListOutput.push(eventByMachRec); //store the totals
+console.log("off hours");			  
+			  machUtilOutputRec = new MachUtilOutputType(
+				"total off hrs",
+				machUtilTable[0].machOffHours.toFixed(2),
+				machUtilTable[1].machOffHours.toFixed(2),
+				machUtilTable[2].machOffHours.toFixed(2),
+				machUtilTable[3].machOffHours.toFixed(2),
+				machUtilTable[4].machOffHours.toFixed(2),
+				machUtilTable[5].machOffHours.toFixed(2),
+				machUtilTable[6].machOffHours.toFixed(2),
+				machUtilTable[7].machOffHours.toFixed(2),
+				machUtilTable[8].machOffHours.toFixed(2)
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
 
-					  eventByMachRec = new eventByMachRecStoreType(
-						" ",
-						" ",
-						" ",
-						" ",
-						" ",
-						" "
-					  );
-					  eventByMachListOutput.push(eventByMachRec); //store the totals
-				  
-					  currMach =  parseInt(response[i].mach_num);
-					  currTotTim = dur_hr;
-					  currOnTim = on_hr;
-					  currOffTim = off_hr;
-				  };
-				  if (i != (response.length-1)){
-					//store record except if it is the last one
-					let eventByMachRec = new eventByMachRecStoreType(
-						response[i].event_str,
-						response[i].start_time_str,
-						response[i].end_time_str,
-						dur_hr.toFixed(3),
-						on_hr.toFixed(3),
-						off_hr.toFixed(3)
-					  );
-					eventByMachListOutput.push(eventByMachRec);
-					};
+			  machUtilOutputRec = new MachUtilOutputType(
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" "
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
+
+			  machUtilOutputRec = new MachUtilOutputType(
+				"percent usage",
+				machUtilTable[0].machPerUsage.toFixed(1)+"%",
+				machUtilTable[1].machPerUsage.toFixed(1)+"%",
+				machUtilTable[2].machPerUsage.toFixed(1)+"%",
+				machUtilTable[3].machPerUsage.toFixed(1)+"%",
+				machUtilTable[4].machPerUsage.toFixed(1)+"%",
+				machUtilTable[5].machPerUsage.toFixed(1)+"%",
+				machUtilTable[6].machPerUsage.toFixed(1)+"%",
+				machUtilTable[7].machPerUsage.toFixed(1)+"%",
+				machUtilTable[8].machPerUsage.toFixed(1)+"%"
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
+
+			  machUtilOutputRec = new MachUtilOutputType(
+				"percent idle",
+				machUtilTable[0].machPerIdle.toFixed(1)+"%",
+				machUtilTable[1].machPerIdle.toFixed(1)+"%",
+				machUtilTable[2].machPerIdle.toFixed(1)+"%",
+				machUtilTable[3].machPerIdle.toFixed(1)+"%",
+				machUtilTable[4].machPerIdle.toFixed(1)+"%",
+				machUtilTable[5].machPerIdle.toFixed(1)+"%",
+				machUtilTable[6].machPerIdle.toFixed(1)+"%",
+				machUtilTable[7].machPerIdle.toFixed(1)+"%",
+				machUtilTable[8].machPerIdle.toFixed(1)+"%"
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
+
+			  machUtilOutputRec = new MachUtilOutputType(
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" ",
+				" "
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
+
+			  machUtilOutputRec = new MachUtilOutputType(
+				"total available hrs",
+				machUtilTable[0].totHours.toFixed(2),
+				machUtilTable[1].totHours.toFixed(2),
+				machUtilTable[2].totHours.toFixed(2),
+				machUtilTable[3].totHours.toFixed(2),
+				machUtilTable[4].totHours.toFixed(2),
+				machUtilTable[5].totHours.toFixed(2),
+				machUtilTable[6].totHours.toFixed(2),
+				machUtilTable[7].totHours.toFixed(2),
+				machUtilTable[8].totHours.toFixed(2)
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
+
+			  machUtilOutputRec = new MachUtilOutputType(
+				"% of total hours",
+				machUtilTable[0].totPerUtil.toFixed(1)+"%",
+				machUtilTable[1].totPerUtil.toFixed(1)+"%",
+				machUtilTable[2].totPerUtil.toFixed(1)+"%",
+				machUtilTable[3].totPerUtil.toFixed(1)+"%",
+				machUtilTable[4].totPerUtil.toFixed(1)+"%",
+				machUtilTable[5].totPerUtil.toFixed(1)+"%",
+				machUtilTable[6].totPerUtil.toFixed(1)+"%",
+				machUtilTable[7].totPerUtil.toFixed(1)+"%",
+				machUtilTable[8].totPerUtil.toFixed(1)+"%"
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
+		
+			  machUtilOutputRec = new MachUtilOutputType(
+				"total workday hrs",
+				machUtilTable[0].workHours.toFixed(2),
+				machUtilTable[1].workHours.toFixed(2),
+				machUtilTable[2].workHours.toFixed(2),
+				machUtilTable[3].workHours.toFixed(2),
+				machUtilTable[4].workHours.toFixed(2),
+				machUtilTable[5].workHours.toFixed(2),
+				machUtilTable[6].workHours.toFixed(2),
+				machUtilTable[7].workHours.toFixed(2),
+				machUtilTable[8].workHours.toFixed(2)
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
+
+			  machUtilOutputRec = new MachUtilOutputType(
+				"% of workday hours",
+				machUtilTable[0].workPer.toFixed(1)+"%",
+				machUtilTable[1].workPer.toFixed(1)+"%",
+				machUtilTable[2].workPer.toFixed(1)+"%",
+				machUtilTable[3].workPer.toFixed(1)+"%",
+				machUtilTable[4].workPer.toFixed(1)+"%",
+				machUtilTable[5].workPer.toFixed(1)+"%",
+				machUtilTable[6].workPer.toFixed(1)+"%",
+				machUtilTable[7].workPer.toFixed(1)+"%",
+				machUtilTable[8].workPer.toFixed(1)+"%"
+			  );
+			  machUtilOutput.push(machUtilOutputRec);
 		
 			  //console.log(videoListOutput);
-			  res.render('report_evm_list', {outputObj: eventByMachListOutput} );
+console.log("before render");			  
+			  res.render('report_mach_util', {outputObj: machUtilOutput} );
 			  //connection.end();
 		  }); //query for read logfiles  
 		});  //query to write to user log	
